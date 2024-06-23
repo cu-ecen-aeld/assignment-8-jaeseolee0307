1501c07a3efe6ba2f74d1e31eaef04cfe94152c8
jaeseolee0307 <jaeseolee0307@gmail.com>
2024-06-23 22:36:17 +0900

    modfy base_external

--- base_external/package/aesd-char/aesd-char.mk
+++ base_external/package/aesd-char/aesd-char.mk
@@ -10,7 +10,7 @@ AESD_CHAR_VERSION = '243082087cff02b7bee1d68d4b7ef981079c9a1f'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
-AESD_CHAR_SITE = 'git@github.com:cu-ecen-aeld/assignments-3-and-later-jaeseolee0307.git
+AESD_CHAR_SITE = 'ggit@github.com:cu-ecen-aeld/assignments-3-and-later-jaeseolee0307.git'
 AESD_CHAR_SITE_METHOD = git
 AESD_CHAR_GIT_SUBMODULES = YES
 
