c0f5eb45fb9876a8a82e894e7be3f8aeacef97ad
jaeseolee0307 <jaeseolee0307@gmail.com>
2024-06-23 23:05:12 +0900

    change git url

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -5,6 +5,7 @@
 #
 ##############################################################
 
+
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
 AESD_ASSIGNMENTS_VERSION = '243082087cff02b7bee1d68d4b7ef981079c9a1f'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
