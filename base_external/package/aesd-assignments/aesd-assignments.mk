51542ad6decc9285fa57b8227f525086472e433c
jaeseolee0307 <jaeseolee0307@gmail.com>
2024-06-23 22:08:19 +0900

    modify script

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -14,7 +14,6 @@ AESD_ASSIGNMENTS_SITE = 'git@github.com:cu-ecen-aeld/assignments-3-and-later-jae
 AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
-
 define AESD_ASSIGNMENTS_BUILD_CMDS
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/finder-app all
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/server all
@@ -32,6 +31,7 @@ define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
     $(INSTALL) -d -m 0755 $(TARGET_DIR)/usr/bin
     $(INSTALL) -m 0755 $(@D)/server/aesdsocket $(TARGET_DIR)/usr/bin
     $(INSTALL) -m 0755 $(@D)/server/aesdsocket-start-stop $(TARGET_DIR)/etc/init.d/S99aesdsocket
+    
 endef
 
 $(eval $(generic-package))
--- base_external/package/aesd-char/aesd-char.mk
+++ base_external/package/aesd-char/aesd-char.mk
@@ -1,21 +1,25 @@
 
 ##############################################################
 #
-# AESDCHAR
+# AESD-ASSIGNMENTS
 #
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESDCHAR_VERSION = '243082087cff02b7bee1d68d4b7ef981079c9a1f'
+AESD_CHAR_VERSION = '243082087cff02b7bee1d68d4b7ef981079c9a1f'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
-AESDCHAR_SITE = 'git@github.com:cu-ecen-aeld/assignments-3-and-later-jaeseolee0307.git'
-AESDCHAR_SITE_METHOD = git
-AESDCHAR_GIT_SUBMODULES = YES
+AESD_CHAR_SITE = 'git@github.com:cu-ecen-aeld/assignments-3-and-later-jaeseolee0307.git
+AESD_CHAR_SITE_METHOD = git
+AESD_CHAR_GIT_SUBMODULES = YES
 
-AESDCHAR_MODULE_SUBDIRS = aesd-char-driver
+AESD_CHAR_LICENSE = Dual BSD/GPL
+#AESD_CHAR_LICENSE_FILES = COPYING
+AESD_CHAR_DEPENDENCIES = linux
+#KERNELDIR = $(LINUX_DIR)
 
+AESD_CHAR_MODULE_SUBDIRS = aesd-char-driver
 
 $(eval $(kernel-module))
 $(eval $(generic-package))
